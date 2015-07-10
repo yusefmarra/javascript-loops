e7e5717eecfd587acf519ff9816f96fef0b9b348
yusefmarra <yusef.marra@gmail.com>
2015-07-10 17:10:37 -0600

    Finished javascript loops exercise

---
+++ script.js
@@ -0,0 +1,48 @@
+for (i = 0; i<=1000;i+=100) {
+  console.log(i);
+}
+
+console.log("\n\n");
+
+for (i=1;i<=128;i=i*2) {
+  console.log(i);
+}
+
+console.log("\n\n");
+
+for (i=0;i<=10;i+=2) {
+  console.log(i);
+}
+
+console.log("\n\n");
+
+for (i=3;i<=15;i+=3) {
+  console.log(i);
+}
+
+console.log("\n\n");
+
+for (i=9;i>=0;i--) {
+  console.log(i);
+}
+
+console.log("\n\n");
+
+for (i=1; i<=4; i++) {
+  var t = 3;
+  while(t) {
+    console.log(i);
+    t--;
+  }
+}
+
+console.log("\n\n");
+
+var t=3
+while (t) {
+  // console.log("0 1 2 3 4");
+  for (i = 0; i<=4; i++) {
+    console.log(i);
+  }
+  t--;
+}
